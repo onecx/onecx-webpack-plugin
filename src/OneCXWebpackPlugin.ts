681f79d00487337050f30195a53239806d8cc12c
markuczy <129275100+markuczy@users.noreply.github.com>
2025-06-17 13:51:08 +0200

    feat: add angular material overwrite

--- src/OneCXWebpackPlugin.ts
+++ src/OneCXWebpackPlugin.ts
@@ -1,26 +1,45 @@
-import { Compiler } from 'webpack';
-const { ModifySourcePlugin, ReplaceOperation } = require('modify-source-webpack-plugin');
+import { Compiler } from "webpack";
+const {
+  ModifySourcePlugin,
+  ReplaceOperation,
+} = require("modify-source-webpack-plugin");
 
 export class OneCXWebpackPlugin {
   apply(compiler: Compiler) {
     const modifyPlugin = new ModifySourcePlugin({
       rules: [
         {
-          test: (module: any) => module.resource?.includes('primeng'),
+          test: (module: any) => module.resource?.includes("primeng"),
           operations: [
             new ReplaceOperation(
-              'all',
-              'document\\.createElement\\(',
+              "all",
+              "document\\.createElement\\(",
               'document.createElementFromPrimeNg({"this": this, "arguments": Array.from(arguments)},'
             ),
             new ReplaceOperation(
-              'all',
-              'Theme.setLoadedStyleName',
-              '(function(_){})'
-            )
-          ]
-        }
-      ]
+              "all",
+              "Theme.setLoadedStyleName",
+              "(function(_){})"
+            ),
+          ],
+        },
+        {
+          test: (module: any) => {
+            return (
+              module.resource &&
+              (module.resource.includes("@angular/material") ||
+                module.resource.includes("@angular/cdk"))
+            );
+          },
+          operations: [
+            new ReplaceOperation(
+              "all",
+              "document\\.createElement\\(",
+              'document.createElementFromMaterial({"this": this, "arguments": Array.from(arguments)},'
+            ),
+          ],
+        },
+      ],
     });
     modifyPlugin.apply(compiler);
   }
